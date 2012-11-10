045bf2c4f966af96c99288f70e0beb3515d7e461
Dan Hammer <dan.s.hammer@gmail.com>
2012-11-10 09:42:06 -0800

    doc string to util function

--- src/clj/empirics/utils.clj
+++ src/clj/empirics/utils.clj
@@ -1,6 +1,10 @@
 (ns empirics.utils
+  "supporting functions for cascalog queries in empirics.core"
   (:use [clojure.contrib.math :only (expt round)]))
 
-(defn round-places [decimals number]
+(defn round-places
+  "custom function to round a fload variable to a certain number of
+  digits; used to shorten lat-lon coordinates."
+  [decimals number]
   (let [factor (expt 10 decimals)]
     (double (/ (round (* factor number)) factor))))
