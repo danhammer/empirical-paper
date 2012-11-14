78f86ac183e9f0f3a6f6ef3da77bd8955660748c
Dan Hammer <dan.s.hammer@gmail.com>
2012-11-13 22:37:51 -0800

    removed graph/drawings/figures from paper.org

--- src/r/analysis.R
+++ src/r/analysis.R
@@ -1,3 +1,4 @@
+library(texreg)
 source("clean-econ.R")
 source("utils.R")
 
@@ -10,9 +11,31 @@ data$post <- ifelse(data$date < as.Date("2011-01-01"), 0, 1)
 
 data <- merge(data, econ.data, by=c("date"))
 
-summary(lm(s.prop ~ 1 + price + cntry*post, data = data))
-summary(lm(s.prop ~ 1 + idn.exch + price + price*cntry*post, data = data))
+data$s.prop <- data$s.prop * 100
+data$price <- data$price / 1000
+data$idn.exch <- data$idn.exch * 1000
 
-summary(lm(total ~ 1 + price + price*cntry*post, data = data))
-summary(lm(total ~ 1 + price + idn.exch, data = data))
+m1 <- lm(s.prop ~ 1 + price + cntry*post, data = data)
+m2 <- lm(s.prop ~ 1 + idn.exch + price + cntry*post, data = data)
+m3 <- lm(s.prop ~ 1 + price*cntry*post, data = data)
+m4 <- lm(s.prop ~ 1 + idn.exch + price*cntry*post, data = data)
 
+table.string <- texreg(list(m1, m2, m3, m4),
+                       model.names=c("(1)", "(2)", "(3)", "(4)"),
+                       digits=3,
+                       table=FALSE,
+                       use.packages=FALSE)
+out <- capture.output(cat(table.string))
+cat(out, file="../../write-up/tables/prop-res.tex", sep="\n")
+
+m1 <- lm(total ~ 1 + cntry*post, data = data)
+m2 <- lm(total ~ 1 + price + cntry*post, data = data)
+m3 <- lm(total ~ 1 + idn.exch + price + cntry*post, data = data)
+
+table.string <- texreg(list(m1, m2, m3),
+                       model.names=c("(1)", "(2)", "(3)"),
+                       digits=3,
+                       table=FALSE,
+                       use.packages=FALSE)
+out <- capture.output(cat(table.string))
+cat(out, file="../../write-up/tables/total-res.tex", sep="\n")
