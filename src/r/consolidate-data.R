0ab92ffbe537dbfb997e92a2aa1533376709704b
Dan Hammer <dan.s.hammer@gmail.com>
2012-11-20 11:51:22 -0800

    removed warping code

--- src/r/consolidate-data.R
+++ src/r/consolidate-data.R
@@ -7,6 +7,13 @@ source("utils.R")
 
 base.dir <- "../../data/processed/empirical-out"
 
+## Read in the characteristics of the land for all observed hits,
+## noting that there is an extraneous index variable in the first
+## column of the raw CSV file.
+land.data <- read.csv("../../data/raw/clean-resamp.csv")
+land.data <- land.data[ ,-1]
+names(land.data) <- c("slope", "accum", "elev", "h", "v", "s", "l")
+
 ## Supporting functions
 
 read.cluster <- function(interval.num, iso, base = base.dir) {
@@ -48,16 +55,39 @@ new.cluster <- function(df) {
   merge(df, y, by=("cid"))
 }
 
+land.characteristics <- function(df) {
+  ## Accepts a data frame from the new.cluster function, and returns a
+  ## list of characteristics associated with the old and new clusters
+  ## in that period.
+  X <- merge(df, land.data, by=c("h", "v", "s", "l"))
+  X <- X[X$new.bin == 1, c("new.cluster", "slope", "accum", "elev")]
+  X.new <- X[X$new.cluster == 1,]
+  X.old <- X[X$new.cluster == 0,]
+  slope <- aggregate(X[["slope"]], by=list(X[["new.cluster"]]), FUN=mean)
+  elev <- aggregate(X[["elev"]], by=list(X[["new.cluster"]]), FUN=mean)
+  accum <- aggregate(X[["accum"]], by=list(X[["new.cluster"]]), FUN=mean)
+  list(old.elev = elev$x[1], new.elev = elev$x[2],
+       old.slope = slope$x[1], new.slope = slope$x[2],
+       old.accum = accum$x[1], new.accum = accum$x[2])
+}
+
 collect.stats <- function(interval.num, iso, data.dir = base.dir) {
   ## Create a "short" data frame with the number of hits in new
   ## clusters and old clusters, as well as the total number of hits in
   ## the 16-day inverval.
   merged <- new.hits(interval.num, iso, base = data.dir)
   x <- new.cluster(merged)
+  screen.sizes <- tail(sort(unique(x[["clcount"]])), 5)
+  print(screen.sizes)
+  x <- x[!(x[["clcount"]] %in% screen.sizes), ]
+  land <- land.characteristics(x)
   new <- nrow(x[x$new.bin == 1 & x$new.cluster == 1, ])
   old <- nrow(x[x$new.bin == 1 & x$new.cluster == 0, ])
   total <- nrow(x[x$new.bin == 1, ])
-  data.frame(new = new, old = old, total = total)
+  data.frame(new = new, old = old, total = total,
+             old.slope = land$old.slope, new.slope = land$new.slope,
+             old.elev = land$old.elev, new.elev = land$new.elev,
+             old.accum = land$old.accum, new.accum = land$new.accum)
 }
 
 compiled.hits <- function(iso, idx.seq, data.dir = base.dir) {
@@ -73,8 +103,8 @@ compiled.hits <- function(iso, idx.seq, data.dir = base.dir) {
 
 ## Count deforestation by cluster type.  This step takes a while,
 ## maybe 5-10 minutes.
-mys <- compiled.hits("mys", 2:155)
-idn <- compiled.hits("idn", 2:155)
+mys <- compiled.hits("mys", 40:155)
+idn <- compiled.hits("idn", 40:155)
 
 ## Append the MYS and IDN data into a single data frame, and screen
 ## out early years for graphing
@@ -93,3 +123,24 @@ png("../../write-up/images/smoothed-prop.png", width=600, height=400)
 g <- ggplot(data = sub.data, aes(x = date, y = s.prop, colour = cntry)) + geom_line()
 (g <- g + xlab("") + ylab(""))
 dev.off()
+
+## Graph indonesian slopes
+png("../../write-up/images/idn-slope.png", width=600, height=400)
+idn.data <- sub.data[sub.data$cntry == "idn", c("date", "new.slope", "old.slope")]
+names(idn.data) <- c("date", "new", "old")
+idn.data <- melt(idn.data, id="date")
+names(idn.data) <- c("date", "cluster.type", "slope")
+g <- ggplot(data = idn.data, aes(x = date, y = slope, colour = cluster.type)) + geom_line()
+(g <- g + xlab("") + ylab(""))
+dev.off()
+
+## Graph malaysian slopes
+png("../../write-up/images/mys-slope.png", width=600, height=400)
+mys.data <- sub.data[sub.data$cntry == "mys", c("date", "new.slope", "old.slope")]
+names(mys.data) <- c("date", "new", "old")
+mys.data <- melt(mys.data, id="date")
+names(mys.data) <- c("date", "cluster.type", "slope")
+g <- ggplot(data = mys.data, aes(x = date, y = slope, colour = cluster.type)) + geom_line()
+(g <- g + xlab("") + ylab(""))
+dev.off()
+
--- src/r/land-analysis.R
+++
@@ -1,9 +0,0 @@
-
-## read in the raw data with land characterstics, tagged to each pixel
-## by its MODIS coordinates
-slope.data <- read.csv("../../data/raw/clean-resamp.csv")
-
-## remove first index column, which is extraneous
-slope.data <- slope.data[,-1]
-
-
